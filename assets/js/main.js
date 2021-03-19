5a55480269ff0f9b763c3bbc11e855e4ebf67fa0
emanmoq <42995029+emanmoq@users.noreply.github.com>
2021-03-19 03:27:25 -0700

    updATE

---
+++ assets/js/main.js
@@ -0,0 +1,112 @@
+jQuery(function ($) {
+  var mainColor="#729D39"
+  //declare slider (owl-carousel slider)
+  $('.galleryCarusel').owlCarousel({
+    autoplay: true,
+    loop: false,
+    rtl: true,
+    nav: true,
+    dotsContainer: '.sliderDots',
+    items: 1,
+  });
+  $('.menuSlider').owlCarousel({
+    autoplay: true,
+    loop: false,
+    rtl: true,
+    nav: true,
+    margin:45,
+    dotsContainer: '.menuDots',
+ resonsive:{
+   0:{
+     items:1,
+   },
+   600:{
+    items:2,
+  },
+  1000:{
+    items:0,
+  }
+ }
+  });
+  $('.clientsSlider').owlCarousel({
+    autoplay: true,
+    loop: false,
+    rtl: true,
+    nav: false,
+    margin:45,
+    dots:true,
+ resonsive:{
+   0:{
+     items:1,
+   },
+   600:{
+    items:2,
+  },
+  1000:{
+    items:0,
+  }
+ }
+  });
+  function onScroll(event) {
+
+    var scrollPosition = $(document).scrollTop();
+    //add class active when select item from navbar 
+    $('.navbar-collapse .nav-item a[href^="#"').each(function () {
+      console.log($(this).attr('href'));
+      var refElement = $($(this).attr("href"));
+      if (refElement.length && refElement.position().top <= scrollPosition) {
+        $('.navbar-collapse .nav-item ').removeClass("active");
+        $(this).parent().addClass("active");
+      }
+      else {
+        $(this).removeClass("active");
+      }
+
+    });
+  }
+  $(document).on("scroll", onScroll);
+  // animate to the section when click  on it
+  $('.navbar-collapse ul li a').on('click', function () {
+    $('html, body').animate({ scrollTop: $(this.hash).offset().top - 5 }, 1000);
+    $('.navbar-collapse.collapse.in').removeClass('in');
+    return false;
+  });
+
+  $(window).scroll(function () {
+    scrollTop = $(window).scrollTop();
+    //add fix header when scroll down and remove when back up
+    if (scrollTop > $('header').height()) {
+      $('header').addClass('scrollNav');
+
+
+    }
+
+    else {
+      $('header').removeClass('scrollNav');
+    }
+
+  });
+  //close menu when choose item
+  $(".navbar .nav-link").click(function () {
+    $(".collapse").removeClass("show");
+  })
+  // declare rating 
+  $(".my-rating").starRating({
+    starSize: 18,
+    rtl: true,
+    activeColor: mainColor,
+    hoverColor: mainColor,
+    ratedColor: mainColor,
+    emptyColor: '#DCDCDC',
+    starShape: 'rounded',
+    useGradient: false,
+    disableAfterRate: false,
+    initialRating: 4,
+    strokeColor: mainColor,
+    readOnly: true,
+
+    callback: function (currentRating, $el) { }
+  });
+  
+});
+
